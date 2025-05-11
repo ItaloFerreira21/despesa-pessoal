58f84d0ec8ccd99ab9a86769be52fe643e692750
italoFerreira21 <dudu21q@gmail.com>
2025-05-10 21:39:39 -0300

    aterando a port

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  console.log(`Aplicação rodando na porta ${port}`);
-
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   const config = new DocumentBuilder()
@@ -31,6 +27,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Aplicação rodando na porta ${port}`);
 }
 bootstrap();
