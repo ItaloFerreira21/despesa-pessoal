30d19296f6ac6508f5e87da12cbba4642df77f7b
italoFerreira21 <dudu21q@gmail.com>
2025-05-10 21:22:34 -0300

    removi duplicações

--- src/despesas/despesas.controller.ts
+++ src/despesas/despesas.controller.ts
@@ -14,33 +14,36 @@ import {
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { DespesasService } from './despesas.service';
 import { CreateDespesaDto } from './dto/create-despesa.dto';
-import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { FiltroDespesaDto } from './dto/filtro-despesa.dto';
 import { UpdateDespesaDto } from './dto/update-despesa.dto';
 
-@Controller('despesas')
+@ApiTags('Despesas')
 @ApiBearerAuth('access-token')
+@UseGuards(JwtAuthGuard)
 @Controller('despesas')
 export class DespesaController {
   constructor(private readonly despesaService: DespesasService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Post()
   @ApiOperation({ summary: 'criar despesa' })
   @ApiResponse({ status: 201, description: 'Despesa criada com sucesso' })
   @ApiResponse({ status: 400, description: 'Dados inválidos' })
   async criarDespesa(@Body() body: CreateDespesaDto, @Request() req) {
-    const userId = req.user.id; // JWT guarda o ID como 'id'
-    console.log(' ID do usuário:', userId);
+    const userId = req.user.id;
     return this.despesaService.criarDespesa(
       body.titulo,
       body.valor,
       userId,
       body.categoriaId,
-    ); //TODO: teste rota com adição de catergoria
+    );
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get()
   @ApiOperation({ summary: 'listar despesas' })
   async listarDespesas(@Request() req, @Query() filtro: FiltroDespesaDto) {
@@ -48,7 +51,6 @@ export class DespesaController {
     return this.despesaService.listarDespesasDoUsuario(userId, filtro);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   @ApiOperation({ summary: 'atualizar despesas' })
   async atualizarDespesa(
@@ -60,7 +62,6 @@ export class DespesaController {
     return this.despesaService.atualizarDespesa(id, userId, body);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   @ApiOperation({ summary: 'deletar despesa' })
   async deletarDespesa(@Param('id', ParseIntPipe) id: number, @Request() req) {
